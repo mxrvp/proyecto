4d9ebc49d47b151476c4316b7d92a996c0289012
Edwin <aneh6272@gmail.com>
2022-06-12 20:55:52 -0500

    probando con pull desde el main

--- proyectoF/src/main/java/com/mycompany/proyectof/ProyectoF.java
+++ proyectoF/src/main/java/com/mycompany/proyectof/ProyectoF.java
@@ -1,14 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
+
 
 package com.mycompany.proyectof;
 
-/**
- *
- * @author admin
- */
+
 public class ProyectoF {
 
     public static void main(String[] args) {
