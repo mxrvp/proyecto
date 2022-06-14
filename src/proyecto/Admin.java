ad265cf314ec87d7fe80c4cd13c92d11db877d9b
max <mxrvp507@hotmail.com>
2022-06-13 22:10:00 -0500

    creado

---
+++ src/proyecto/Admin.java
@@ -0,0 +1,36 @@
+
+package proyecto;
+
+public class Admin {
+
+    private String usuario = "Edwin";
+    private String contrasena = "Hola";
+
+    public Admin() {}
+    
+    public Admin(String usuario, String contrasena) {
+        this.usuario = usuario;
+        this.contrasena = contrasena;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    
+    
+    
+    
+}
